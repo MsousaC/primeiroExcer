63bc7d234ac97e92cde563b090afcac96eccaba1
MsousaC <marciioocandido@gmail.com>
2023-04-20 07:40:57 -0300

    BuscandoConhecimentos

---
+++ aula27.py
@@ -0,0 +1,12 @@
+"""
+Fatiamento de strings
+012345678
+Olá Mundo
+-987654321
+Fatiamento [i:f:p][::]
+OBS. A função LEN retorna a QTD
+de caracteres da str
+"""
+
+variavel = 'Ola mundo'
+print(variavel[2])
---
+++ aula29.py
@@ -0,0 +1,6 @@
+"""
+Introcução ao try/except
+TRY -> Tentar executar o código
+EXCEPT -> Ocorreu algum erro ao tentar executar
+"""
+
